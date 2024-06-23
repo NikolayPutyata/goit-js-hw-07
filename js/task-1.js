87ce09ccbfaec5c5b4917390e882777eb6474a05
NikolayPutyata <nikolayputyata@gmail.com>
2024-06-24 00:11:53 +0300

    jk

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,1 @@
-const list = document.querySelector(".categories");
-const items = document.querySelectorAll(".item");
-console.log(items.textContent);
-console.log(list.classList.add("fdsfdsfsd"));
-console.log(list.classList);
-console.log("aaaa");
+console.log("gfrgdf");
