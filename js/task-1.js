2ff965e2747158d746015a9c0732e1b11d6052a7
NikolayPutyata <nikolayputyata@gmail.com>
2024-06-24 00:04:39 +0300

    gfh

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+const list = document.querySelector(".categories");
+const items = document.querySelectorAll(".item");
+console.log(items.textContent);
+console.log(list.classList.add("fdsfdsfsd"));
+console.log(list.classList);
+console.log("aaaa");
