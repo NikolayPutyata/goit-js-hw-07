c217cbdcf36d2af3a4401204c79e0948bb8353dc
NikolayPutyata <nikolayputyata@gmail.com>
2024-06-24 10:04:00 +0300

    step 1 hw1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,11 @@
-console.log("gfrgdf");
+const mainList = document.querySelector("#categories");
+const mainListItems = document.querySelectorAll(".item");
+const mainListItem = document.querySelector(".item");
+//1
+console.log(`Number of categories: ${mainListItems.length}`);
+//2
+mainListItems.forEach((item) => {
+  console.log(`Categories: ${1}`);
+  console.log(`Elements: ${1}`);
+});
+console.log(mainListItem);
