8a95aebb45631f9947e38b712a745deb11a2b823
NikolayPutyata <nikolayputyata@gmail.com>
2024-06-24 18:10:47 +0300

    step 1 hw5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const btnChangeColor = document.querySelector(".change-color");
+const textBcgColor = document.querySelector(".color");
+
+btnChangeColor.addEventListener("click", handlerFunction);
+
+function handlerFunction() {
+  const randomColor = getRandomHexColor();
+  textBcgColor.textContent = randomColor;
+  body.style.backgroundColor = randomColor;
+}
