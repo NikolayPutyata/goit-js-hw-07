fdfa99522a6e3109518192c459a68e5f35fa3de9
NikolayPutyata <nikolayputyata@gmail.com>
2024-06-24 17:44:12 +0300

    step 1 hw4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handlerFunction);
+
+function handlerFunction(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+  }
+  const userInformation = {
+    email,
+    password,
+  };
+  console.log(userInformation);
+  form.reset();
+}
