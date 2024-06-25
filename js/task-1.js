8d42bc16bc34558af5270d601882c265bda8b6f3
NikolayPutyata <nikolayputyata@gmail.com>
2024-06-25 13:52:19 +0300

    final step hw1

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,8 @@
-const mainList = document.querySelector("#categories");
 const mainListItems = document.querySelectorAll(".item");
-const mainListItem = document.querySelector(".item");
-//1
+
 console.log(`Number of categories: ${mainListItems.length}`);
-//2
+
 mainListItems.forEach((item) => {
-  console.log(`Categories: ${1}`);
-  console.log(`Elements: ${1}`);
+  console.log(`Categories: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
-console.log(mainListItem);
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,21 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnCreate = document.querySelector("#controls button[data-create]");
+const btnDestroy = document.querySelector("#controls button[data-destroy]");
+const input = document.querySelector("#controls input");
+const boxes = document.querySelector("#boxes");
+
+input.addEventListener("input", (event) => {
+  if (event.currentTarget.value >= 1 && event.currentTarget.value <= 100) {
+    const userNumber = event.currentTarget.value;
+    console.log(userNumber);
+  }
+  
+});
+
+btnCreate.addEventListener("click", createBoxes());
+btnDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {}
+function destroyBoxes() {}
