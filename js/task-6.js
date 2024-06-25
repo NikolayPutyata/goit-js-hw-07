9f3760cca168adf2a52abe08b7f11546015f95e5
NikolayPutyata <nikolayputyata@gmail.com>
2024-06-25 14:33:18 +0300

    step 1 hw6

--- js/task-6.js
+++ js/task-6.js
@@ -3,21 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
 const btnCreate = document.querySelector("#controls button[data-create]");
 const btnDestroy = document.querySelector("#controls button[data-destroy]");
 const input = document.querySelector("#controls input");
 const boxes = document.querySelector("#boxes");
 
-input.addEventListener("input", (event) => {
-  if (event.currentTarget.value >= 1 && event.currentTarget.value <= 100) {
-    const userNumber = event.currentTarget.value;
-    console.log(userNumber);
+btnCreate.addEventListener("click", () => {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
   }
-  
+  input.value = "";
 });
 
-btnCreate.addEventListener("click", createBoxes());
 btnDestroy.addEventListener("click", destroyBoxes);
 
-function createBoxes(amount) {}
-function destroyBoxes() {}
+function createBoxes(amount) {
+  boxes.innerHTML = ""; // Очищає попередні елементи
+  const elements = [];
+  for (let i = 0; i < amount; i++) {
+    const newElement = document.createElement("div");
+    newElement.style.width = `${30 + i * 10}px`;
+    newElement.style.height = `${30 + i * 10}px`;
+    newElement.style.backgroundColor = getRandomHexColor();
+    elements.push(newElement);
+  }
+  boxes.append(...elements);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = ""; // Очищає вміст div#boxes
+}
